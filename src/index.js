290a83a1e0d66b90bc79bed0ab97814b18e9b1b0
Pedro Sousa <pedrossm2000@gmail.com>
2022-03-17 14:24:18 -0300

    Add Date picker

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import { Provider } from 'react-redux';
-import store from './store';
-import App from './App';
-import '@fontsource/roboto/300.css'
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import { Provider } from "react-redux";
+import store from "./store";
+import App from "./App";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { LocalizationProvider } from "@mui/lab";
+import AdapterDateFns from "@mui/lab/AdapterDateFns"
 
 ReactDOM.render(
   <BrowserRouter>
-    <Provider store={ store }>
-      <App />
+    <Provider store={store}>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <App />
+      </LocalizationProvider>
     </Provider>
   </BrowserRouter>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
-
