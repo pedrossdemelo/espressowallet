974e6bd5e56b777232ef3efb45a95761d2ef8ffe
Pedro Sousa <pedrossm2000@gmail.com>
2022-03-19 01:52:22 -0300

    tweak color

--- src/components/History.js
+++ src/components/History.js
@@ -67,23 +67,23 @@ export const iconsMap = {
 };
 
 export const colorMap = {
+  Transportation: "#6366f1",
+  Prize: "#2196f3",
+  Education: "#7dd3fc",
+  Savings: "#2dd4bf",
+  Salary: "#047857",
   Investments: "#22c55e",
+  Eletronics: "#a3e635",
   Payment: "#fbbf24",
   Food: "#fb923c",
-  Work: "#3b82f6",
-  Education: "#7dd3fc",
-  Entertainment: "#ec4899",
-  Transportation: "#6366f1",
-  Eletronics: "#a3e635",
+  Work: "#b45309",
   Health: "#dc2626",
   Clothing: "#f43f5e",
+  Gift: "#f87171",
+  Entertainment: "#ec4899",
+  Sale: "#d946ef",
   Household: "#a855f7",
   Other: "#d4d4d4",
-  Salary: "#047857",
-  Savings: "#2dd4bf",
-  Gift: "#f87171",
-  Prize: "#2196f3",
-  Sale: "#d946ef",
 };
 
 export default function History() {
